f0ac2306bc94acd8dbdce56e018775e7e5801e24
DualDen <disa.shilow2@gmail.com>
2022-12-09 08:40:43 +0700

    dsa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import queryString from "query-string"
+import Game from "./components/Game";
 
 
 
@@ -9,20 +9,7 @@ import queryString from "query-string"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game/>
     </div>
   );
 }
---
+++ src/components/Game.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import {io} from "socket.io-client";
+
+const Game = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default Game;
